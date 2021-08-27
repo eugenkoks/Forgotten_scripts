be577185662f8d5f903441b2ee149f0f8cfb3521
evgenkoks <82104842+evgenkoks@users.noreply.github.com>
2021-08-28 00:11:15 +0300

    all_refactored, headless is now work

--- Forgotten_scripts.py
+++ Forgotten_scripts.py
@@ -1,7 +1,7 @@
 import os
 import pickle
 import time
-from random import random
+import random
 
 import eel
 import lxml
@@ -34,30 +34,38 @@ class Forgotten_script:
         self.like = '/html/body/div/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/section/div/div/div[1]/div/div[1]/article/div/div/div/div[3]/div[5]/div/div[3]/div'
         self.retweet = '/html/body/div/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/section/div/div/div[1]/div/div[1]/article/div/div/div/div[3]/div[5]/div/div[2]/div'
         self.retweet_confirm = '/html/body/div/div/div/div[1]/div[2]/div/div/div/div[2]/div[3]/div/div/div/div'
-        self.subscribe_button = '/html/body/div/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[1]/div[1]/div[1]/div/div[3]/div/div'
+        self.subscribe_button = '/html/body/div/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[1]/div[1]/div[1]/div/div[3]'
         self.start_page = 'https://twitter.com/login'
-        options = webdriver.ChromeOptions()
-        options.add_argument('--ignore-certificate-errors')
-        options.add_argument('--incognito')
-        options.add_argument("javascript.enabled")
-        options.add_argument(
+        self.headless_options = webdriver.ChromeOptions()
+        self.headless_options.add_argument('--ignore-certificate-errors')
+        self.headless_options.add_argument('--incognito')
+        self.headless_options.add_argument("javascript.enabled")
+        self.headless_options.add_argument('--disable-gpu')
+        self.headless_options.add_argument('--no-sandbox')
+        self.headless_options.add_argument('--disable-dev-shm-usage')
+        self.headless_options.add_argument('--headless')
+        self.headless_options.add_argument(
             "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.123 Safari/537.36")
-        if headless == "on":
-            options.add_argument('--headless')
-        else:
-            options.add_argument('--window-size=800,900')
+        self.normal_options = webdriver.ChromeOptions()
+        self.normal_options.add_argument('--ignore-certificate-errors')
+        self.normal_options.add_argument('--incognito')
+        self.normal_options.add_argument('javascript.enabled')
+        self.normal_options.add_argument('--disable-gpu')
+        self.normal_options.add_argument('--no-sandbox')
+        self.normal_options.add_argument('--disable-dev-shm-usage')
+        self.normal_options.add_argument('--window-size=800,900')
+        self.normal_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.123 Safari/537.36")
         if tag == 'on':
             pass
         else:
             pass
-        self.driver = webdriver.Chrome(options=options)
-        self.driver.wait = WebDriverWait(self.driver, 3)
 
     def scrapping_tweet(self, url):
-        self.driver.get(url)
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, self.tweet_page)))
-        tweet_id = self.driver.find_element_by_xpath(self.tweet_id).get_attribute('id')
-        html = self.driver.page_source
+        driver = webdriver.Chrome(options=self.headless_options)
+        driver.get(url)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, self.tweet_page)))
+        tweet_id = driver.find_element_by_xpath(self.tweet_id).get_attribute('id')
+        html = driver.page_source
         soup = BeautifulSoup(html, "lxml")
         all_link = soup.find(id=tweet_id).find_all('a')
         links = []
@@ -66,78 +74,75 @@ class Forgotten_script:
                 continue
             else:
                 links.append(link.get('href'))
+        print(links)
+        driver.quit()
         return links
 
-    def check_url(self, url):
-        self.driver.get(url)
-        chk_url = self.driver.current_url
-        if chk_url != url:
-            return False
-        else:
-            return True
-
-    def login(self, max_try=5):
-        while not self.check_url(self.start_page) or max_try == 0:
-            self.check_url(self.start_page)
-            max_try = max_try - 1
+    def login(self):
+        driver = webdriver.Chrome(options=self.normal_options)
+        driver.get(self.start_page)
+        time.sleep(random.randint(3, 5))
         try:
-            login_input_wait = WebDriverWait(self.driver, 10).until(
+            login_input_wait = WebDriverWait(driver, 10).until(
                 EC.element_to_be_clickable((By.XPATH, self.login_input)))
-            pass_input_wait = WebDriverWait(self.driver, 10).until(
+            pass_input_wait = WebDriverWait(driver, 10).until(
                 EC.element_to_be_clickable((By.XPATH, self.password_input)))
-            auth_btn_wait = WebDriverWait(self.driver, 10).until(
+            auth_btn_wait = WebDriverWait(driver, 10).until(
                 EC.element_to_be_clickable((By.XPATH, self.authorize_btn)))
             login_input_wait.send_keys(self.login_str)
             pass_input_wait.send_keys(self.password_str)
             auth_btn_wait.click()
         except NoSuchElementException:
-            pass
+            print('NoSuchElementException')
         except TimeoutException:
-            pass
+            print('TimeoutException')
+        cookies = driver.get_cookies()
+        print("Успешная авторизация")
+        driver.quit()
+        return cookies
 
-    def subscribe(self, links):
+    def like_rt(self, cookies):
+        driver = webdriver.Chrome(options=self.normal_options)
+        driver.get(self.url)
+        for cookie in cookies:
+            driver.add_cookie(cookie)
+        driver.refresh()
+        time.sleep(random.randint(3, 5))
+        like_btn_wait = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, self.like)))
+        rt_btn_wait = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, self.retweet)))
+        like_btn_wait.click()
+        rt_btn_wait.click()
+        rt_confirm_btn_wait = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, self.retweet_confirm)))
+        rt_confirm_btn_wait.click()
+
+    def subscribe(self, links, cookies):
+        driver = webdriver.Chrome(options=self.normal_options)
         for link in links:
             link = "https://twitter.com" + link
-            while not self.check_url(link):
-                self.check_url(link)
-            try:
-                while WebDriverWait(self.driver, 3).until(
-                        EC.visibility_of_element_located(
-                            (By.XPATH, "/html/body/div/div/div/div[1]/div/div[1]/div/div"))):
-                    self.driver.refresh()
-            except TimeoutException:
-                try:
-                    subscribe_button_wait = WebDriverWait(self.driver, 10).until(
-                        EC.element_to_be_clickable((By.XPATH, self.subscribe_button)))
-                    subscribe_button_option = self.driver.find_element_by_xpath(
-                        '/html/body/div/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[1]/div[1]/div[1]/div/div[4]/div/div').get_attribute(
-                        "data-testid").split('-')
-                    if subscribe_button_option[1] == 'follow':
-                        subscribe_button_wait.click()
-                except TimeoutException:
-                    print('Не удалось прожать подписку')
-
-    def like_rt(self, url):
-        self.driver.get(url)
-        tweet_page_wait = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.XPATH,
-                                                                                                '/html/body/div/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/section/div/div/div[1]/div/div[1]/article/div/div/div/div[3]/div[5]')))
-        if tweet_page_wait:
-            like_btn_wait = WebDriverWait(self.driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, self.like)))
-            rt_btn_wait = WebDriverWait(self.driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, self.retweet)))
-            like_btn_wait.click()
-            rt_btn_wait.click()
-            rt_confirm_btn_wait = WebDriverWait(self.driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, self.retweet_confirm)))
-            rt_confirm_btn_wait.click()
-        else:
-            self.like_rt(url)
+            driver.get(link)
+            time.sleep(random.randint(3, 5))
+            for cookie in cookies:
+                driver.add_cookie(cookie)
+            driver.refresh()
+            time.sleep(random.randint(3, 5))
+            subscribe_button_wait = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, self.subscribe_button)))
+            print(subscribe_button_wait)
+            subscribe_button_option = driver.find_element_by_xpath(
+                '//div[@data-testid="placementTracking"]/div/div').get_attribute(
+                "data-testid").split('-')
+            print(subscribe_button_option[1])
+            if subscribe_button_option[1] == 'follow':
+            fa    driver.find_element_by_xpath(self.subscribe_button).click()
 
 
 start = Forgotten_script('https://twitter.com/torpedoAIO/status/1411769304048091142', 'qawsedrftgy1973@', '89955086949',
                          'off', 'off')
-test = start.scrapping_tweet('https://twitter.com/torpedoAIO/status/1411769304048091142')
-start.login()
-start.like_rt('https://twitter.com/torpedoAIO/status/1411769304048091142')
-start.subscribe(test)
+# скраппинг работает и хедлесс
+links = start.scrapping_tweet('https://twitter.com/torpedoAIO/status/1411769304048091142')
+cookies = start.login()
+# start.like_rt(cookies)
+start.subscribe(links, cookies)
